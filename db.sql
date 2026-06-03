ccdf0aa68a056b0b3489c79b21c90f0391aabcb0
dms1e4 <simone.esposito4@proton.me>
2026-06-03 17:49:57 +0200

    Update db.sql
    creata tabella utente, modifica al commento iniziale

--- db.sql
+++ db.sql
@@ -1,1 +1,19 @@
--- db relazione per brickcaveau
+-- db relazionale per brickcaveau
+
+DROP SCHEMA IF EXISTS BrickCaveau;
+CREATE SCHEMA BrickCaveau;
+USE BrickCaveau;
+
+CREATE TABLE Utente(
+	ID_Utente INT AUTO_INCREMENT PRIMARY KEY,
+    Nome VARCHAR(30) NOT NULL,
+    Cognome VARCHAR(30) NOT NULL,
+    Email VARCHAR(35) NOT NULL UNIQUE,
+    Password VARCHAR(255) NOT NULL,
+    Telefono VARCHAR(15) NOT NULL UNIQUE,
+    is_Admin BOOLEAN
+);
+
+
+
+
